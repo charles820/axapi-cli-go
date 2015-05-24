6f1a630f683ae6573ea745ef3e5af257c30ce148
Koichiro Den <den@fout.jp>
2015-05-25 01:06:29 +0900

    first commit

---
+++ config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type AxDevice struct {
+	Ip       string
+	Username string
+	Password string
+}
+
+type Config struct {
+	AxDevice AxDevice `json:"ax_device"`
+}
+
+var ConfigData Config
+
+func ConfigInit(filepath ...string) {
+	if filepath == nil || filepath[0] != "" {
+		file, err := ioutil.ReadFile(filepath[0])
+		if err != nil {
+			fmt.Printf("Could not read config JSON file: %v\n", filepath)
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		err = json.Unmarshal(file, &ConfigData)
+		if err != nil {
+			fmt.Println("JSON file might not conform to required format.")
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		// it's highly recommended to hardcode your device info here
+		// rather than using config JSON file for ease and security.
+		ConfigData = Config{
+			AxDevice: AxDevice{
+				Ip:       "10.10.10.10",
+				Username: "admin",
+				Password: "admin",
+			},
+		}
+	}
+}
